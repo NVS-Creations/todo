87893925889671734b6767f1d612cb879e3aad0c
nvs <sibiselvanallusamy123@gmail.com>
2021-01-20 14:57:20 +0530

    store

--- lib/controller/controller.dart
+++ lib/controller/controller.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 class TodoController extends GetxController {
+  final box = GetStorage();
   TextEditingController to = TextEditingController();
   bool checkbox = false;
-  List<String> todos = [];
-  List<String> completed = [];
+  List todos = [];
+  List completed = [];
   void check() {
     checkbox = !checkbox;
     update();
@@ -13,23 +15,36 @@ class TodoController extends GetxController {
 
   void add() async {
     todos.add(to.text);
+    await box.write("todos", todos);
+    to.clear();
     update();
   }
 
   void remove(int index) async {
     todos.removeAt(index);
+    await box.write("todos", todos);
     update();
   }
 
   void completedfun(int index) async {
     if (completed.contains(todos[index])) {
       completed.remove(todos[index]);
+      box.write("completed", completed);
     } else {
       completed.add(todos[index]);
+      box.write("completed", completed);
     }
     update();
   }
-
+getfromstore()async{
+    if (box.hasData("todos")) {
+      todos = box.read("todos");
+    }
+    if (box.hasData("completed")) {
+      completed = box.read("completed");
+    }
+    update();
+}
   checked(int index) {
     if (completed.contains(todos[index])) {
       return true;
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
+import 'package:get_storage/get_storage.dart';
+
 import './views/home.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import './controller/controller.dart';
 
-main() => runApp(MyApp());
+main() async {
+  await GetStorage.init();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   final todoController = Get.put(TodoController());
   @override
   Widget build(BuildContext context) {
+    todoController.getfromstore();
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       home: Home(),
